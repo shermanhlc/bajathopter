34127b4b3cb6579895312b377627bc5137458b6b
shermanhlc <shermanhlc@proton.me>
2024-11-20 19:21:21 -0600

    experiment, may be reversed

--- core/components/core_component/inc/fart.h
+++ core/components/core_component/inc/fart.h
@@ -2,20 +2,12 @@
 #define FART_H
 
 #include <stdio.h>
-// #include "esp_wifi.h"
-
+#include "esp_wifi.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
-/* BLE */
-#include "nimble/nimble_port.h"
-#include "nimble/nimble_port_freertos.h"
-#include "host/ble_hs.h"
-#include "host/util/util.h"
-#include "console/console.h"
-#include "services/gap/ble_svc_gap.h"
-// #include "blecent.h"
 
 void hello(void);
+
+void tup(void);
 
 
 #endif // FART_H
