0fc1f6607cd9e1446a9c2648b297b4002c3251de
shermanhlc <shermanhlc@proton.me>
2024-11-20 19:47:19 -0600

    able to use c++

--- core/components/core_component/inc/fart.h
+++ core/components/core_component/inc/fart.h
@@ -1,8 +1,16 @@
 #ifndef FART_H
 #define FART_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 
 void hello(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // FART_H
---
+++ core/main/main.cpp
@@ -0,0 +1,8 @@
+#define TAG "MAIN"
+
+// #include "esp_log.h"x
+#include "fart.h"
+
+extern "C" void app_main() {
+    hello();
+}
